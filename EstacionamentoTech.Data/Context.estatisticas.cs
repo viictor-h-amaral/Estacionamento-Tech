8d9089e4b7bdc2c96a2508c7acc9d1a532ae65c8
viictor-h-amaral <victorhamaralom@gmail.com>
2025-11-16 14:31:09 -0300

    criação dos tiles de estatísticas

---
+++ EstacionamentoTech.Data/Context.estatisticas.cs
@@ -0,0 +1,133 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace EstacionamentoTech.Data
+{
+    public partial class Context
+    {
+        public long RetornarMediaEstacionamentosPorCliente()
+        {
+            string strComando = $@"SELECT AVG(D.ESTACIONAMENTOS) 
+                                    FROM (
+	                                    SELECT COUNT(A.ID) ESTACIONAMENTOS
+	                                    FROM {dataBaseName}.HISTORICO_ESTACIONAMENTOS AS A
+                                        INNER JOIN {dataBaseName}.VEICULOS AS B ON A.VEICULO = B.ID
+                                        INNER JOIN {dataBaseName}.CLIENTES AS C ON B.CLIENTE = C.ID
+                                        WHERE A.ENTRADA >= DATE_SUB(NOW(), INTERVAL 30 DAY)
+                                        GROUP BY C.ID
+                                    ) AS D";
+
+            try
+            {
+                _connection.Open();
+                var comando = new MySqlCommand(strComando, _connection);
+
+                var resultado = comando.ExecuteScalar();
+                return Convert.ToInt32(resultado);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        public decimal RetornarMediaTempoEfetivoEstacionamentos()
+        {
+            string strComando = $@"SELECT AVG(B.DURACAO)
+                                    FROM (
+                                        SELECT
+                                            TIMESTAMPDIFF(MINUTE, A.ENTRADA, A.SAIDA) DURACAO 
+                                        FROM 
+                                            {dataBaseName}.HISTORICO_ESTACIONAMENTOS AS A
+                                        WHERE 
+                                            A.SAIDA IS NOT NULL
+                                            AND A.ENTRADA >= DATE_SUB(NOW(), INTERVAL 30 DAY)
+                                    ) AS B";
+
+            try
+            {
+                _connection.Open();
+                var comando = new MySqlCommand(strComando, _connection);
+
+                var resultado = comando.ExecuteScalar();
+                return Convert.ToDecimal(resultado);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        public decimal RetornarMediaTempoCobradoEstacionamentos()
+        {
+            string strComando = $@"SELECT AVG(B.MINUTOSCOBRADOS)
+                                    FROM (
+                                        SELECT
+                                            (A.HORASCOBRADAS * 60 ) MINUTOSCOBRADOS
+                                        FROM 
+                                            {dataBaseName}.HISTORICO_ESTACIONAMENTOS AS A
+                                        WHERE 
+                                            A.SAIDA IS NOT NULL
+                                            AND A.ENTRADA >= DATE_SUB(NOW(), INTERVAL 30 DAY)
+                                    ) AS B";
+
+            try
+            {
+                _connection.Open();
+                var comando = new MySqlCommand(strComando, _connection);
+
+                var resultado = comando.ExecuteScalar();
+                return Convert.ToDecimal(resultado);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        public string RetornarClienteComMaiorNumeroDeEstacionamentos()
+        {
+            string strComando = $@"SELECT RANQUEAMENTO_CLIENTES.NOME
+                                    FROM (
+                                        SELECT 
+                                            A.NOME,
+                                            COUNT(C.ID) AS QUANTIDADE_ESTACIONAMENTOS,
+                                            RANK() OVER (ORDER BY COUNT(C.ID) DESC) AS RANKING
+                                        FROM CLIENTES AS A
+                                        INNER JOIN VEICULOS AS B ON B.CLIENTE = A.ID
+                                        INNER JOIN HISTORICO_ESTACIONAMENTOS AS C ON C.VEICULO = B.ID
+                                        WHERE C.ENTRADA >= DATE_SUB(NOW(), INTERVAL 30 DAY)
+                                        GROUP BY A.NOME
+                                    ) AS RANQUEAMENTO_CLIENTES
+                                    WHERE RANKING = 1";
+
+            try
+            {
+                _connection.Open();
+                var comando = new MySqlCommand(strComando, _connection);
+
+                var resultado = comando.ExecuteScalar();
+                return Convert.ToString(resultado);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
--- EstacionamentoTech.MVC/Controllers/HomeController.cs
+++ EstacionamentoTech.MVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using EstacionamentoTech.Data;
 using EstacionamentoTech.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,15 +8,54 @@ namespace EstacionamentoTech.MVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private Context _contexto;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _contexto = new Context();
         }
 
         public IActionResult Index()
         {
+            var mediaEstacionamentosCliente = CalcularMediaEstacionamentosPorCliente();
+            var mediaTempoEfetivoEstacionamentos = CalcularMediaTempoEfetivoEstacionamentos();
+            var mediaTempoCobradoEstacionamentos = CalcularMediaTempoCobradoEstacionamentos();
+            var clienteComMaisEstacionamentos = RetornarClienteComMaiorNumeroDeEstacionamentos();
+
+            ViewBag.MediaEstacionamentosCliente = mediaEstacionamentosCliente.ToString();
+            ViewBag.MediaTempoEfetivoEstacionamentos = ConverterDecimalEmTempo(mediaTempoEfetivoEstacionamentos);
+            ViewBag.MediaTempoCobradoEstacionamentos = ConverterDecimalEmTempo(mediaTempoCobradoEstacionamentos);
+            ViewBag.ClienteComMaisEstacionamentos = clienteComMaisEstacionamentos;
+
             return View();
+        }
+
+        private long CalcularMediaEstacionamentosPorCliente()
+        {
+            return _contexto.RetornarMediaEstacionamentosPorCliente();
+        }
+
+        private decimal CalcularMediaTempoEfetivoEstacionamentos()
+        {
+            return Math.Round(_contexto.RetornarMediaTempoEfetivoEstacionamentos());
+        }
+
+        private decimal CalcularMediaTempoCobradoEstacionamentos()
+        {
+            return Math.Round(_contexto.RetornarMediaTempoCobradoEstacionamentos());
+        }
+
+        private string ConverterDecimalEmTempo(decimal minutos)
+        {
+            return  minutos > 60
+                    ?   $"{Math.Round(minutos / 60, 0)}h{minutos % 60}min"
+                    :   $"{minutos}min";
+        }
+
+        private string RetornarClienteComMaiorNumeroDeEstacionamentos()
+        {
+            return _contexto.RetornarClienteComMaiorNumeroDeEstacionamentos();
         }
 
         public IActionResult Privacy()
